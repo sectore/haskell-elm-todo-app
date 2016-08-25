f68454a2296b11424b74b42a114e9eb8253a2d34
sectore <info@websector.de>
2016-08-25 21:16:21 +0200

    remove user model (deprecated)

--- server/src/Models.hs
+++ server/src/Models.hs
@@ -18,30 +18,15 @@ import GHC.Generics
 import Database.Persist.TH
 
 share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
-  User
-    name Text
-    age  Int
-    UniqueName name
-    deriving Eq Read Show
   Todo
     description String
     completed Bool
-    deriving Generic Show Eq
+    deriving Generic Show Read Eq
 |]
-
-instance FromJSON User where
-  parseJSON = withObject "User" $ \ v ->
-    User <$> v .: "name"
-         <*> v .: "age"
-
-instance ToJSON User where
-  toJSON (User name age) =
-    object [ "name" .= name
-           , "age"  .= age]
 
 instance FromJSON Todo where
   parseJSON = withObject "Todo" $ \ v ->
     Todo <$> v .: "description"
          <*> v .: "completed"
-         
+ 
 instance ToJSON Todo
