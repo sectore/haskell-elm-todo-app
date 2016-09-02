3847f1ee0206c55eb9eff142cb448fc3f6becc7a
sectore <info@websector.de>
2016-09-02 15:29:04 +0200

    use elm-mdl

--- client/src/Main.elm
+++ client/src/Main.elm
@@ -6,6 +6,11 @@ import Http
 import Json.Decode as Decode exposing (Decoder, (:=))
 import Json.Decode.Pipeline as Pipeline
 import Task
+import Material
+import Material.Layout as Layout
+import Material.Scheme as Scheme
+import Material.Color as Color
+import Material.List as Lists
 
 
 -- import Html.Attributes exposing (..)
@@ -29,6 +34,7 @@ main =
 type alias Model =
     { todos : List Todo
     , selectedTodo : Maybe Todo
+    , mdl : Material.Model
     }
 
 
@@ -39,9 +45,17 @@ type alias Todo =
     }
 
 
+initialModel : Model
+initialModel =
+    { mdl = Material.model
+    , todos = []
+    , selectedTodo = Nothing
+    }
+
+
 init : ( Model, Cmd Msg )
 init =
-    ( Model [] Nothing, getTodos )
+    ( initialModel, getTodos )
 
 
 
@@ -49,13 +63,17 @@ init =
 
 
 type Msg
-    = FetchTodosDone (List Todo)
+    = Mdl (Material.Msg Msg)
+    | FetchTodosDone (List Todo)
     | FetchTodosFail Http.Error
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
+        Mdl msg' ->
+            Material.update msg' model
+
         FetchTodosDone todos ->
             { model | todos = todos } ! []
 
@@ -101,14 +119,34 @@ todoDecoder =
 
 todoView : Todo -> Html Msg
 todoView todo =
-    li []
+    Lists.li []
         [ text todo.todoDescription ]
 
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ ul
-            []
-            (List.map todoView model.todos)
+    Layout.render Mdl
+        model.mdl
+        [ Layout.fixedHeader
         ]
+        { header = header model
+        , drawer = []
+        , tabs = ( [], [] )
+        , main =
+            [ div []
+                [ Lists.ul
+                    []
+                    (List.map todoView model.todos)
+                ]
+            ]
+        }
+        -- mdl color scheme
+        |> Scheme.topWithScheme Color.Brown Color.Amber
+
+
+header : Model -> List (Html Msg)
+header model =
+    [ Layout.row []
+        [ Layout.title [] [ text "Haskell Elm Todo App" ]
+        ]
+    ]
